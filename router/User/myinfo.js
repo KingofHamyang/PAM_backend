660e95421a20ee22d74bd1ec6333e1d66479d250
KingofHamyang <kjds1235@naver.com>
2019-08-13 17:50:16 +0900

    my_info router is changed to Promise, async function

--- router/User/myinfo.js
+++ router/User/myinfo.js
@@ -2,18 +2,42 @@ var mysql = require("mysql");
 const config = require("../../DB_config");
 
 var msq = mysql.createConnection(config);
+
+function my_msq(query_string, data_input) {
+  return new Promise((resolve, reject) => {
+    console.log("promise 접근");
+    msq.query(query_string, data_input, (err, response, fields) => {
+      if (err) {
+        reject(fields);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+async function myinfo_query(req, res) {
+  var result = await my_msq(
+    "SELECT * from userinfo where ID = ?",
+    req.session.userID
+  );
+
+  return result;
+}
+
 var myinfo = (req, res) => {
-  result = {};
   console.log("myinfo 접근");
   if (req.session.userID) {
-    msq.query(
-      "SELECT * from userinfo where ID = ?",
-      req.session.userID,
-      (err, response, fields) => {
+    myinfo_query(req, res)
+      .then(response => {
         console.log(response);
+        console.log("promise 성공");
         res.send(response);
-      }
-    );
+      })
+      .catch(err => {
+        console.log(err);
+        res.send(err);
+      });
   } else {
     res.send("로그인 후에 이용해주세요.");
   }
